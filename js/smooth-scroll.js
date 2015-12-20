6690b6da4890d8da2d5d9134cd6c21a95aa8d9ad
wanakijiji <wanakijiji@users.noreply.github.com>
2015-12-20 21:07:33 +0900

    Added fadein on scroll function

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -2,14 +2,41 @@
 
 $(function(){
 
+	'use strict';
+
+	var scrollSpeed = 1000, // スクロール時間（ミリ秒）
+			buttonOffset = 200; // トップへ戻る固定ボタンを表示するスクロール量（px）
+
 	// スムーズスクロール
-	$('a[href^=#]').on('click', function() {
-		var speed = 1000; // スクロール時間（ミリ秒）
-		var href= $(this).attr('href');
-		var target = $((href == '#top' && !$('#top').length) || href == '#' || href == '' ? 'html' : href);
-		var position = target.offset().top;
-		$('body,html').animate({scrollTop:position}, speed, 'swing');
-		return false;
+	$('a[href^=#]').on( 'click', function() {
+		var href= $(this).attr( 'href' );
+		if ( href !== '#' || href !== '' ) {
+			var target = ( href === '#top' && !$('#top').length ) ? 'html' : href,
+					position = $(target).offset().top;
+			$('body, html').animate( {scrollTop:position}, scrollSpeed, 'swing' );
+			return false;
+		}
 	});
+	// トップへ戻る固定ボタンの表示・非表示
+	if ( $('.scroll-fade').length ) {
+		var sfBtn = $('.scroll-fade'),
+				sfBtnDisplay = '',
+				flagAnimate = false;
+		$(sfBtn).css( 'display', 'none' );
+		$(window).on( 'scroll', function() {
+			sfBtnDisplay = sfBtn.css( 'display' );
+			if ( $(this).scrollTop() > buttonOffset && sfBtnDisplay === 'none' && !flagAnimate ) {
+				flagAnimate = true;
+				sfBtn.fadeIn(function() {
+					flagAnimate = false;
+				});
+			} else if ( $(this).scrollTop() <= buttonOffset && sfBtnDisplay !== 'none' && !flagAnimate )  {
+				flagAnimate = true;
+				sfBtn.fadeOut(function() {
+					flagAnimate = false;
+				});
+			}
+		});
+	}
 
 });

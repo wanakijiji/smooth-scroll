3cf2cba2be0f2498312443c4043cf6ec61e6301d
Wana Kijiji <aroundit11@gmail.com>
2016-05-15 09:35:05 +0900

    fixed bugs.

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -1,42 +1,43 @@
 // JavaScript Document
 
-$(function(){
-
+(function($) {
 	'use strict';
 
-	var scrollSpeed = 1000, // スクロール時間（ミリ秒）
-			buttonOffset = 200; // トップへ戻る固定ボタンを表示するスクロール量（px）
-
-	// スムーズスクロール
-	$('a[href^=#]').on( 'click', function() {
-		var href= $(this).attr( 'href' );
-		if ( href !== '#' || href !== '' ) {
-			var target = ( href === '#top' && !$('#top').length ) ? 'html' : href,
-					position = $(target).offset().top;
-			$('body, html').animate( {scrollTop:position}, scrollSpeed, 'swing' );
-			return false;
-		}
-	});
-	// トップへ戻る固定ボタンの表示・非表示
-	if ( $('.scroll-fade').length ) {
-		var sfBtn = $('.scroll-fade'),
-				sfBtnDisplay = '',
-				flagAnimate = false;
-		$(sfBtn).css( 'display', 'none' );
-		$(window).on( 'scroll', function() {
-			sfBtnDisplay = sfBtn.css( 'display' );
-			if ( $(this).scrollTop() > buttonOffset && sfBtnDisplay === 'none' && !flagAnimate ) {
-				flagAnimate = true;
-				sfBtn.fadeIn(function() {
-					flagAnimate = false;
-				});
-			} else if ( $(this).scrollTop() <= buttonOffset && sfBtnDisplay !== 'none' && !flagAnimate )  {
-				flagAnimate = true;
-				sfBtn.fadeOut(function() {
-					flagAnimate = false;
-				});
+	$(function() { 
+		var scrollSpeed = 1000, // スクロール時間（ミリ秒）
+				buttonOffset = 200; // トップへ戻る固定ボタンを表示するスクロール量（px）
+	
+		// スムーズスクロール
+		$('a[href^="#"]').on( 'click', function() {
+			var href= $(this).attr( 'href' );
+			if ( href !== '#' || href !== '' ) {
+				var target = ( href === '#top' && !$('#top').length ) ? 'html' : href,
+						position = $(target).offset().top;
+				$('body, html').animate( {scrollTop:position}, scrollSpeed, 'swing' );
+				return false;
 			}
 		});
-	}
+		// トップへ戻る固定ボタンの表示・非表示
+		if ( $('.scroll-fade').length ) {
+			var sfBtn = $('.scroll-fade'),
+					sfBtnDisplay = '',
+					flagAnimate = false;
+			$(sfBtn).css( 'display', 'none' );
+			$(window).on( 'scroll', function() {
+				sfBtnDisplay = sfBtn.css( 'display' );
+				if ( $(this).scrollTop() > buttonOffset && sfBtnDisplay === 'none' && !flagAnimate ) {
+					flagAnimate = true;
+					sfBtn.fadeIn(function() {
+						flagAnimate = false;
+					});
+				} else if ( $(this).scrollTop() <= buttonOffset && sfBtnDisplay !== 'none' && !flagAnimate )  {
+					flagAnimate = true;
+					sfBtn.fadeOut(function() {
+						flagAnimate = false;
+					});
+				}
+			});
+		}
+	});
 
-});
+})( jQuery );

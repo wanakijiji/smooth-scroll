3a116d9eb90cbf2f5ccd46a89fec00a912c6a3f2
Wana Kijiji <wanakijiji@users.noreply.github.com>
2020-11-26 10:31:01 +0900

    Refactored all code

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -1,43 +1,37 @@
 // JavaScript Document
 
 (function($) {
-	'use strict';
 
 	$(function() { 
-		var scrollSpeed = 1000, // スクロール時間（ミリ秒）
-				buttonOffset = 200; // トップへ戻る固定ボタンを表示するスクロール量（px）
+    const scrollSpeed = 1000; // スクロール時間（ミリ秒）
+    const	buttonOffset = 200; // トップへ戻る固定ボタンを表示するスクロール量（px）
+    const fixedHeaderOffset = false; // 固定ヘッダー用スクロール量調整ON/OFF（true/false）
+    const additionalOffset = 0; // スクロール量調整（px）
 	
 		// スムーズスクロール
-		$('a[href^="#"]').on( 'click', function() {
-			var href= $(this).attr( 'href' );
-			if ( href !== '#' || href !== '' ) {
-				var target = ( href === '#top' && !$('#top').length ) ? 'html' : href,
-						position = $(target).offset().top;
-				$('body, html').animate( {scrollTop:position}, scrollSpeed, 'swing' );
-				return false;
-			}
-		});
-		// トップへ戻る固定ボタンの表示・非表示
-		if ( $('.scroll-fade').length ) {
-			var sfBtn = $('.scroll-fade'),
-					sfBtnDisplay = '',
-					flagAnimate = false;
-			$(sfBtn).css( 'display', 'none' );
-			$(window).on( 'scroll', function() {
-				sfBtnDisplay = sfBtn.css( 'display' );
-				if ( $(this).scrollTop() > buttonOffset && sfBtnDisplay === 'none' && !flagAnimate ) {
-					flagAnimate = true;
-					sfBtn.fadeIn(function() {
-						flagAnimate = false;
-					});
-				} else if ( $(this).scrollTop() <= buttonOffset && sfBtnDisplay !== 'none' && !flagAnimate )  {
-					flagAnimate = true;
-					sfBtn.fadeOut(function() {
-						flagAnimate = false;
-					});
-				}
-			});
-		}
+    $('a[href^="#"]').on('click', function() {
+      const href= $(this).attr('href');
+      const headerHeight = (fixedHeaderOffset) ? $('header').outerHeight() : 0;
+      if (href !== '#' || href !== '') {
+        const target = (href === '#top' && ! $('#top').length) ? 'html' : href;
+        const	position = $(target).offset().top - headerHeight - additionalOffset;
+        $('body, html').animate({scrollTop: position}, scrollSpeed, 'swing');
+        return false;
+      }
+    });
+    // トップへ戻る固定ボタンの表示・非表示
+    const sfBtn = $('.scroll-fade');
+    if (sfBtn.length) {
+      $(sfBtn).css('display', 'none');
+      $(window).on('scroll', function() {
+        const sfBtnDisplay = sfBtn.css('display');
+        if ($(this).scrollTop() > buttonOffset && sfBtnDisplay === 'none') {
+          sfBtn.stop().fadeIn();
+        } else if ($(this).scrollTop() <= buttonOffset && sfBtnDisplay !== 'none')  {
+          sfBtn.stop().fadeOut();
+        }
+      });
+    }
 	});
 
-})( jQuery );
+})(jQuery);
